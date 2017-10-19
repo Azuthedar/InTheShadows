b35e763eaa863c55ab677110a929c2de9faa7708
Johann BUYS <jbuys@e4r23p5.wethinkcode.co.za>
2017-10-19 07:52:00 +0200

    Changed room UV's and added in DayNightCycle

---
+++ Assets/_Scripts/DayNightCycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+	// Use this for initialization
+
+	[SerializeField]
+	private float _speed = 2f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (Vector3.right * this._speed * Time.deltaTime);
+
+	}
+}
